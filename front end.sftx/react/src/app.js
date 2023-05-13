c64eb38e58612457051dcc1d0218092bee78c64a
Guilherme Vitor <109116903+zeppelito@users.noreply.github.com>
2023-05-13 16:09:36 -0300

    Add files via upload

---
+++ front end.sftx/react/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Este é o meu primeiro programa React.</p>
+    </div>
+  );
+}
+
+export default App;
